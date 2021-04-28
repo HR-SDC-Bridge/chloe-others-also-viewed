3972adfa36a5cc811485363980c754498e6a1c09
Chloe Tan <chloe.tan95@gmail.com>
2021-04-28 19:57:19 -0400

    Commented out CRUD plan

--- server/server.js
+++ server/server.js
@@ -23,4 +23,15 @@ app.get('/similar-products-by-views/:id', (req, res) => {
     .catch((err) => console.error('Unable to get similar items by views: ', err));
 });
 
+/*
+Create / POST - create a new item:
+  Create new records in the db (Client needs to provide an array of ID's for the similar_items field. DB should auto increment the id field.)
+Read / GET - read an item:
+  DONE - /similar-products-by-views/:id
+Update / PUT - update an item:
+  Update existing records in the db (Client needs to provide the id it wants to update and an array of ID's to update the document with)
+Delete / DELETE - delete an item:
+  Delete existing record (Client needs to provide the id it wants to delete - possibly allow client to send an array of id's if it wants to delete multiple at a time)
+ */
+
 module.exports = app;
